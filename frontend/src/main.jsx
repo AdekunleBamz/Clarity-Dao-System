f0c17f06f79b0267d56b5edc68338e0475c7376e
Developer <developer@example.com>
2026-01-26 14:45:18 +0100

    feat(theme): Add dark mode toggle with system preference detection and persistence

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { WalletProvider } from './context/WalletContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <WalletProvider>
-        <App />
-      </WalletProvider>
+      <ThemeProvider>
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
